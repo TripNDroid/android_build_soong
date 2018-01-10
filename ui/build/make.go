00ecc1c3924b967c59c76278cb40d31cd888a766
TripNRaVeR <noerialexeew@live.nl>
2018-01-10 15:53:07 +0100

    soong: add tripndroid version dumpvar

--- ui/build/make.go
+++ ui/build/make.go
@@ -102,6 +102,7 @@ func runMakeProductConfig(ctx Context, config Config) {
 	bannerVars := []string{
 		"PLATFORM_VERSION_CODENAME",
 		"PLATFORM_VERSION",
+		"TD_VERSION",
 		"TARGET_PRODUCT",
 		"TARGET_BUILD_VARIANT",
 		"TARGET_BUILD_TYPE",
